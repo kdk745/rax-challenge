0c2f08953dd5837c3d2178d48deb829eb5393f98
Kayne Khoury <kaynekhoury1@gmail.com>
2018-03-10 11:54:11 -0600

    more cleanup

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Author: Kayne Khoury
 #### Will list and sort an array of posts by clicking on a column header
 #### Front End components built with Material-UI
+#### Actions are stored in src/actions
+#### Reducers are stored in src/reducers
--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
 
-const oldFetch = window.fetch;
-
-window.fetch = function fetch(url, settings) {
-  const headers = Object.assign(settings ? settings.headers : {},
-  {authorization: localStorage.getItem("token")});
-  settings = settings || {};
-  settings.headers = headers;
-  return oldFetch(url, settings);
-};
-
 ReactDOM.render(
   <Provider store={store}>
     <App id="App" />
